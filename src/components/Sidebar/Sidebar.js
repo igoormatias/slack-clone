4466ff8e0e7133549134013da3c1efa63e7888d0
Igor Matias <igorhmatias@gmail.com>
2020-08-12 12:47:53 -0300

    connect sidebar to firestore

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React,{ useState,useEffect} from 'react';
+
+import db from '../../services/firebase'
 
 import SidebarOptions from '../SidebarOptions/SidebarOptions'
 
@@ -18,6 +20,21 @@ import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import AddIcon from '@material-ui/icons/Add';
 
 function Sidebar() {
+    const [ channels, setChannels] = useState([])
+
+    useEffect(()=>{
+        db.collection('rooms').onSnapshot(snapshot =>(
+        setChannels(
+            snapshot.docs.map(doc =>({
+                id: doc.id,
+                name: doc.data().name
+
+            }))
+        )
+            
+        ))
+
+    },[])
   return (
       <div className="sidebar">
           <div className="sidebar__header">
@@ -45,6 +62,10 @@ function Sidebar() {
             <SidebarOptions Icon={ExpandMoreIcon} title="Channels" />
             <hr />
             <SidebarOptions Icon={AddIcon} title="Add Channel" />
+            {channels.map(channel =>(
+                <SidebarOptions  title={channel.name} id={channel.id} />
+
+            ))}
          </div>
   ); 
 }
